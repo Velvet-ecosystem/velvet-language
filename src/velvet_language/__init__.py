7d46d9288d0d56a65da38f8febfc688806ac8a88
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-08-10 00:31:01 -0700

    Expose planner and selector public API

--- src/velvet_language/__init__.py
+++ src/velvet_language/__init__.py
@@ -1,5 +1,18 @@
 """Velvet Language public surface."""
 
 from .fallback import FallbackExpression, render_fallback
+from .models import MeaningPacket, RenderedExpression, ResponseVariant
+from .planner import ConversationPlan, plan_response
+from .selector import SelectionContext, select_response
 
-__all__ = ["FallbackExpression", "render_fallback"]
+__all__ = [
+    "ConversationPlan",
+    "FallbackExpression",
+    "MeaningPacket",
+    "RenderedExpression",
+    "ResponseVariant",
+    "SelectionContext",
+    "plan_response",
+    "render_fallback",
+    "select_response",
+]
