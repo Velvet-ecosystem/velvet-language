0c80b8f94f855e67dfb8ed227c8c8cf09ec281d1
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-08-10 00:40:40 -0700

    Expose sentence frame API

--- src/velvet_language/__init__.py
+++ src/velvet_language/__init__.py
@@ -1,6 +1,15 @@
 """Velvet Language public surface."""
 
 from .fallback import FallbackExpression, render_fallback
+from .frames import (
+    MissingSlotError,
+    RealizedFrame,
+    SentenceFrame,
+    UnknownSlotError,
+    choose_and_realize,
+    infer_slots,
+    realize_frame,
+)
 from .models import MeaningPacket, RenderedExpression, ResponseVariant
 from .planner import ConversationPlan, plan_response
 from .selector import SelectionContext, select_response
@@ -9,10 +18,17 @@ __all__ = [
     "ConversationPlan",
     "FallbackExpression",
     "MeaningPacket",
+    "MissingSlotError",
+    "RealizedFrame",
     "RenderedExpression",
     "ResponseVariant",
     "SelectionContext",
+    "SentenceFrame",
+    "UnknownSlotError",
+    "choose_and_realize",
+    "infer_slots",
     "plan_response",
+    "realize_frame",
     "render_fallback",
     "select_response",
 ]
